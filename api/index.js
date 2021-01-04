3e889191e2fdbb78c45cdec09925bb38c5464e8a
ECWireless <Elliott@valtechcreative.com>
2021-01-04 16:33:17 -0700

    Clean up API

--- api/index.js
+++ api/index.js
@@ -4,10 +4,6 @@ const { ChainId, Token, WETH, Trade, TokenAmount, TradeType, Fetcher, Percent, R
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send('CypherShares API.');
-});
-
 app.get('/api/buy_price/:quantity/:currency/:input_type', cors(), async (req, res) => {
 	try {
 		if (req.method === "OPTIONS") {
@@ -16,22 +12,20 @@ app.get('/api/buy_price/:quantity/:currency/:input_type', cors(), async (req, re
 
 		const quantity = req.params.quantity
 		const quantityLarge = quantity * 10**18
-		const currency = req.params.currency
-		const input_type = req.params.input_type
+		// const currency = req.params.currency
+		// const input_type = req.params.input_type
 	
-		const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from the current Unix time
 		const WETH_ADDRESS = '0xd0A1E359811322d97991E03f863a0C30C2cF029C'
 		const CSDEFI_TOKEN_ADDRESS = '0xf9d50338Fb100B5a97e79615a8a912e10975b61c'
 	
-		const csDEFI = new Token(ChainId.KOVAN, CSDEFI_TOKEN_ADDRESS, 18)
-	
-		const pair = await Fetcher.fetchPairData(csDEFI, WETH[csDEFI.chainId])
-		const route = new Route([pair], WETH[csDEFI.chainId])
-		const trade = new Trade(route, new TokenAmount(WETH[csDEFI.chainId], quantityLarge), TradeType.EXACT_INPUT)
-	
-		const slippageTolerance = new Percent('1', '10000') // 50 bips, or 0.50%
-		const amountOutMin = (trade.minimumAmountOut(slippageTolerance).toFixed(18) * 10**18).toString()
-		const amountReadable = trade.minimumAmountOut(slippageTolerance).toFixed(18).toString()
+		const csDEFI = new Token(ChainId.KOVAN, CSDEFI_TOKEN_ADDRESS, 18);
+		const pair = await Fetcher.fetchPairData(csDEFI, WETH[csDEFI.chainId]);
+		const route = new Route([pair], WETH[csDEFI.chainId]);
+		const trade = new Trade(route, new TokenAmount(WETH[csDEFI.chainId], quantityLarge), TradeType.EXACT_INPUT);
+		const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from the current Unix time
+		const slippageTolerance = new Percent('1', '10000'); // 1 bips, or 0.01%
+		const amountOutMin = (trade.minimumAmountOut(slippageTolerance).toFixed(18) * 10**18).toString();
+		const amountReadable = trade.minimumAmountOut(slippageTolerance).toFixed(18).toString();
 
 		const data = {
 			"buy_price": {
@@ -68,12 +62,8 @@ app.get('/api/buy_price/:quantity/:currency/:input_type', cors(), async (req, re
 	}
 });
 
-// app.get('/api', cors(), (req, res) => {
-//     return res.json({"buy_price":{"amount_in":"1000000000000000","amount_out":"7057266332547125","path":["0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2","0x1494CA1F11D487c2bBe4543E90080AeBa4BA3C2b"],"deadline":1609727670,"gas_cost":"170000","gas_price":"127170610158","slippage":"-0.006031912846003326964441243396256458922966089","trade_type":"exact_in","display":{"from_quantity":"0.001","from_token_price_usd":"$971.65","to_quantity":"0.007057","to_token_price_usd":"$138.51","input_value_usd":"$0.97","output_value_usd":"$0.98","total_price_currency":"0.022619 ETH","total_price_usd":"$21.98","gas_price_eth":"0.021619 ETH","gas_price_usd":"$21.01","slippage":"\u003c 0.01%"}}});
-// });
-
 app.listen(process.env.PORT || 3001, () =>
-  console.log(`Example app listening on port 3001!`),
+  console.log(`Listening on port 3001!`),
 );
 
-module.exports = app
+module.exports = app;
