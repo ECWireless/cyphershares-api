ea6b77ff27b95e0d4797be18fb4b828f7ef3459f
ECWireless <Elliott@valtechcreative.com>
2021-01-04 13:54:41 -0700

    re-add allowCors

--- api/index.js
+++ api/index.js
@@ -4,6 +4,23 @@ const { ChainId, Token, WETH, Trade, TokenAmount, TradeType, Fetcher, Percent, R
 
 const app = express();
 
+const allowCors = fn => async (req, res) => {
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	// another common pattern
+	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+	res.setHeader(
+	  'Access-Control-Allow-Headers',
+	  'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	)
+	if (req.method === 'OPTIONS') {
+	  res.status(200).end()
+	  return
+	}
+	return await fn(req, res)
+}
+
 app.get('/', (req, res) => {
     return res.send('CypherShares API.');
 });
@@ -76,4 +93,4 @@ app.listen(process.env.PORT || 3001, () =>
   console.log(`Example app listening on port 3001!`),
 );
 
-module.exports = app
+module.exports = allowCors(app)
