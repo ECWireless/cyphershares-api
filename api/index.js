5aa2dc938cecf37e36a865fe0c7b2185946a2b0e
ECWireless <Elliott@valtechcreative.com>
2021-01-08 22:22:44 -0700

    Turn amount_in to string

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.get('/api/buy_price/:quantity/:currency/:input_type', cors(), async (req, re
 
 		const data = {
 			"buy_price": {
-				"amount_in": quantityLarge,
+				"amount_in": quantityLarge.toString(),
 				"amount_out": amountOut,
 				"path": [
 					WETH_ADDRESS,
@@ -85,9 +85,10 @@ app.get('/api/sell_price/:quantity/:currency/:input_type', cors(), async (req, r
 		const CSDEFI_TOKEN_ADDRESS = '0xf9d50338Fb100B5a97e79615a8a912e10975b61c'
 
 		const csDEFI = new Token(ChainId.KOVAN, CSDEFI_TOKEN_ADDRESS, 18);
-		const pair = await Fetcher.fetchPairData(csDEFI, WETH[csDEFI.chainId]);
-		const route = new Route([pair], WETH[csDEFI.chainId]);
-		const trade = new Trade(route, new TokenAmount(WETH[csDEFI.chainId], quantityLarge), TradeType.EXACT_INPUT);
+		const pair = await Fetcher.fetchPairData(WETH[csDEFI.chainId], csDEFI);
+		const route = new Route([pair], csDEFI);
+		const trade = new Trade(route, new TokenAmount(csDEFI, quantityLarge), TradeType.EXACT_INPUT);
+
 		const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from the current Unix time
 		const slippageTolerance = new Percent('50', '10000'); // 50 bips, or 0.5%
 		const amountOut = (trade.outputAmount.toFixed(18) * 10**18).toString();
@@ -100,17 +101,16 @@ app.get('/api/sell_price/:quantity/:currency/:input_type', cors(), async (req, r
 		}
 
 		const data = {
-			"buy_price": {
-				"amount_in": quantityLarge,
+			"sell_price": {
+				"amount_in": quantityLarge.toString(),
 				"amount_out": amountOut,
 				"path": [
-					WETH_ADDRESS,
-					CSDEFI_TOKEN_ADDRESS
+					CSDEFI_TOKEN_ADDRESS,
+					WETH_ADDRESS
 				],
 				"deadline": deadline,
 				"gas_cost": "202776",
-				"gas_price": "20000000000",
-				"trade_type": "exact_in",
+				"gas_price": "30000000000",
 				"slippage": slippageNumber,
 				"display": {
 					"from_quantity": quantity,
@@ -121,7 +121,7 @@ app.get('/api/sell_price/:quantity/:currency/:input_type', cors(), async (req, r
 					"output_value_usd": "$NA",
 					"total_price_currency":"0.022619 ETH",
 					"total_price_usd":"$NA",
-					"gas_price_eth":"0.004056 ETH",
+					"gas_price_eth":"0.000169 ETH",
 					"gas_price_usd":"$NA",
 					"slippage": slippage
 				}
